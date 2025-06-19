9766dcf2ea48b5553c8f467c563f32ae64d58974
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2025-06-19 12:42:22 +0530

    Update db.php

--- admin/photo-admin/db.php
+++ admin/photo-admin/db.php
@@ -4,3 +4,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+
+//<?php
+//$conn = new mysqli("localhost", "u315669183_soe2", "Sa5msa5m@", "u315669183_soe2");
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
+//?>
